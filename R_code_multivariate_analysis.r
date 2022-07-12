11251bc8c3b9f8133037225930fabb842c3075fb
GiadaFranci <100956811+GiadaFranci@users.noreply.github.com>
2022-07-12 17:10:34 +0200

    Update R_code_multivariate_analysis.r

--- R_code_multivariate_analysis.r
+++ R_code_multivariate_analysis.r
@@ -8,8 +8,8 @@ library(patchwork)
 
 setwd("c:/lab/")
 
-#misura della varaibilita su una ariabile, qunid si deve scegliere una delle bande 
-#per scegliere la migliore o uso una banda di cui ho fiducia (es. NIR) oppure calcolarmi indici spettrali
+#misura della varaibilita su una variabile, qunid si deve scegliere una delle bande 
+#per scegliere la migliore o uso una banda di cui ho fiducia (es. NIR) oppure mi calccolo gli indici spettrali
 #(es. NDVI) opure posso compattare tutto in una sola banda (analsisi multivariata)
 #e prendere solo la prima banda
 
@@ -19,7 +19,7 @@ setwd("c:/lab/")
 #vari tipi di analisi multivariata 
 
 #come pasare da un immagine a più dimensioni ad una a una dimensione?
-#se io mi trovo nella bi dimensione non concepisco le terza manon è detto che degli elemnti non lo siano solo
+#se io mi trovo nella bi dimensione non concepisco le terza ma non è detto che degli elemnti non lo siano solo
 #che non li vediamo come tali
 #ipercubo nel quadro di dalì
 #la prima banda è quella con più info e quindi usiamo quella per il calcolo della variabilità
@@ -28,7 +28,7 @@ p224r63_2011 <- brick("p224r63_2011_masked.grd")
 plot(p224r63_2011) #sistema a 7 bande l'immagine lendsat
 
 #prima di analisi multivariata facciamo il ricampionamento o resambling
-#diminuisco la risoluzione della mia immaine con la mouving winfow, aggreggo dei pizel 
+#diminuisco la risoluzione della mia immaine con la mouving window, aggreggo dei pizel 
 # e ne formo con meno dettaglio. la funzione per fare quetso è detta AGGREGATE
 #fa dei summary statistics. fact sta per factor cioè di quanto aggerghiamo i pixel 
 #es. se è 10 compatto 10pixel x 10 pixel
