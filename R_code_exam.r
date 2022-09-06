d070c9648d7e92a0369ebaf94776d69f0d72c2b8
GiadaFranci <100956811+GiadaFranci@users.noreply.github.com>
2022-09-06 16:50:18 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -9,13 +9,28 @@
 # In the second part of my project I look at the LST, of the various periods, to understand how the disappearance of
 # the trees altered the temperatures in the area douring the current period.
 
-# Data include all the 10 mts resolution bands avaiable: RGB.
+# Data include all the 10 mts resolution bands avaiable: RGB and NIR.
 
 # NAMES OF THE USED BANDS:
 
 #  T55HEU_20191119T001109_TCI_10m.jp2
 #   T55HEU_20200227T001109_TCI_10m.jp2
 #    T55HEU_20220122T001111_TCI_10m.jp2
+
+#  T55HEU_20191119T001109_B02_10m.jp2
+#   T55HEU_20191119T001109_B03_10m.jp2
+#    T55HEU_20191119T001109_B04_10m.jp2
+#     T55HEU_20191119T001109_B08_10m.jp2
+
+#  T55HEU_20200227T001109_B02_10m.jp2
+#   T55HEU_20200227T001109_B03_10m.jp2
+#    T55HEU_20200227T001109_B04_10m.jp2
+#     T55HEU_20200227T001109_B08_10m.jp2
+
+#  T55HEU_20220122T001111_B02_10m.jp2
+#   T55HEU_20220122T001111_B03_10m.jp2
+#    T55HEU_20220122T001111_B04_10m.jp2
+#     T55HEU_20220122T001111_B08_10m.jp2
 
 #  S3A_SL_2_LST____20190531T233505.SEN3
 #   S3A_SL_2_LST____20200102T233500.SEN3
@@ -37,26 +52,48 @@ library(RColorBrewer)
 
 setwd("D:/fire_data")
 
-#blu banda 1
-#verde banda 2
-#rosso banda 3
+# blue band 1
+# green band 2
+# red band 3
+# infrared band 4
 
-lg2019 <- brick("2019.jpg")
-lg2019
+# 2019
+rlist_19 <- list.files(pattern = "2019_B") 
+rlist_19
 
-lg2020 <- brick("2020.jpg")
-lg2020
+import_19 <- lapply(rlist_19, raster)
+import_19
 
-lg2022 <- brick("2022.jpg")
-lg2022
+tgr_19 <- stack(import_19)
+tgr_19
 
-# collect images in the same window
+plotRGB(tgr_19, r=4, g=3, b=2, stretch="lin")
 
-pg19 <- ggRGB(lg2019, r=1, g=2, b=3, stretch="lin")
-pg20 <- ggRGB(lg2020, r=1, g=2, b=3, stretch="lin")
-pg22 <- ggRGB(lg2022, r=1, g=2, b=3, stretch="lin")
+# 2020
+rlist_20 <- list.files(pattern = "2020_B") 
+rlist_20
 
-pg19/pg20/pg22
+import_20 <- lapply(rlist_20, raster)
+import_20
+
+tgr_20 <- stack(import_20)
+tgr_20
+
+plotRGB(tgr_20, r=4, g=3, b=2, stretch="lin")
+
+# 2022
+rlist_22 <- list.files(pattern = "2022_B") 
+rlist_22
+
+import_22 <- lapply(rlist_22, raster)
+import_22
+
+tgr_22 <- stack(import_22)
+tgr_22
+
+plotRGB(tgr_22, r=4, g=3, b=2, stretch="lin")
+
+#considering that i put the infrared band on red, everything that appears red in the image is vegetation
 
 # ## LAND COVER
 
@@ -64,29 +101,29 @@ pg19/pg20/pg22
 
 dev.off()
 
-lg2019c <- unsuperClass(lg2019, nClasses=4)
+lg2019c <- unsuperClass(tgr_19, nClasses=4)
 plot(lg2019c$map)
 lg2019c
-# class 1: clouds
-# class 2: bare ground
-# class 3: cultivated land
-# class 4: woodland
+# class 1: clouds + bare ground
+# class 2: clouds
+# class 3: woodland 
+# class 4: woodland 
 
-lg2020c <- unsuperClass(lg2020, nClasses=4)
+lg2020c <- unsuperClass(tgr_20, nClasses=4)
 plot(lg2020c$map)
 lg2020c
-# class 1: bare ground
-# class 2: burned area
+# class 1: burned area
+# class 2: bare ground
 # class 3: woodland
 # class 4: cultivated land
 
-lg2022c <- unsuperClass(lg2022, nClasses=4)
+lg2022c <- unsuperClass(tgr_22, nClasses=4)
 plot(lg2022c$map)
 lg2022c
-# class 1: cultivated land + recovery area
-# class 2: bare ground
-# class 3: clouds
-# class 4: woodland
+# class 1: clouds
+# class 2: woodland
+# class 3: bare ground+ ex burned area
+# class 4: cultivated land  
 
 # Visualizing this data has been useful to obtain maps where I have pixels of green areas (woods),
 # so I can begin to calculate the occupied area and the proportion of pixels of green areas in the year 2019
@@ -97,67 +134,62 @@ lg2022c
 
 # ### FREQUENCY CALCULATION 
 
-# total pixels 92001420 divided like this:
+# total pixels 91982062 divided like this:
 
 freq(lg2019c$map)
 #value  count:
-#classe 1   818023 pixel
-#classe 2 10563469 pixel
-#classe 3 20264754 pixel 
-#classe 4 60355174 pixel 
+#classe 1  14343988 pixel
+#classe 2    976465 pixel
+#classe 3  36821671 pixel 
+#classe 4  39839938 pixel 
 
 freq(lg2020c$map)
 #value  count:
-#classe 1  3397852 pixel
-#classe 2 29784744 pixel
-#classe 3 43757344 pixel
-#classe 4 15061480 pixel 
+#classe 1  31156016 pixel
+#classe 2  10451394 pixel
+#classe 3  46232134 pixel
+#classe 4   4142518 pixel 
 
 freq(lg2022c$map)
 #value  count:
-#classe 1 18657949 pixel
-#classe 2  5995202 pixel
-#classe 3   288324 pixel
-#classe 4 67059945 pixel 
+#classe 1    112573 pixel
+#classe 2  42704248 pixel
+#classe 3  34768672 pixel
+#classe 4  14396569 pixel 
 
 # Woods distribution and percentage for years 2019, 2020 and 2022.
 
-tot19 <- 92001420
-prop_green_19 <- 60355174 / tot19
-percent_green_19 <- 60355174 * 100 / tot19
-#percent 65,602437
+tot19 <- 91982062
+tot_green <- 36821671  + 39839938
+# = 76661609
+prop_green_19 <- 76661609 / tot19
+percent_green_19 <- 76661609 * 100 / tot19
+#percent 83,326547
 
-tot20 <- 92001420
-prop_green_20 <- 43757344 / tot20
-percent_green_20 <- 43757344 * 100 / tot20
-#percent 47,561596
+tot20 <- 91982062
+prop_green_20 <- 46232134 / tot20
+percent_green_20 <- 46232134 * 100 / tot20
+#percent 50,251543
 
-tot22 <- 92001420
-prop_green_22 <- 67059945 / tot22
-percent_green_22 <- 67059945 * 100 / tot22
-#percent 72,890119
-
-# burned area in 2020
-
-tot20 <- 92001420
-prop_fire_20 <- 29784744 / tot20
-percent_fire_20 <- 29784744 * 100 / tot20
-#percent 32,374222
+tot22 <- 91982062
+prop_green_22 <- 42704248 / tot22
+percent_green_22 <- 42704248 * 100 / tot22
+#percent 46,416944
 
 # calculation of the total trees, lost throughout the Australia, taking into account
 # the WWF data that report about 19 million hectares of woods impacted by the fire.
 
-green_lost <- 60355174 - 43757344 
-# = 16597830
+green_lost <- 76661609 - 46232134 
+# = 30429475
 
-percent_green_lost <- ( green_lost / 60355174) * 100
-#percent 27,500260
+percent_green_lost <- ( green_lost / 76661609) * 100
+#percent 39,693231
 
 # ### DATAFRAME
 
 class <- c("2019", "2020")
-green_perc_19_20 <- c(65.602437, 47.561596)
-green_perc_19_22 <- c(65.602437, 72.890119)
+green_perc_19_20 <- c(83.326547, 50.251543)
+green_perc_19_22 <- c(83.326547, 46.416944)
 
 # we build the table with the function data.frame
 
@@ -181,7 +213,6 @@ ggplot(multitemporal, aes(x=class, y=green_perc_19_22, color=class)) +
 
 
 ## 2 # LAND SURFACE TEMPERATURE ANALYSIS
-
 # we use the variable LST (Land Surface Temperature) of Copernicus
 # to see how much the temperature in Australia has changed
 # before after and during the fire
